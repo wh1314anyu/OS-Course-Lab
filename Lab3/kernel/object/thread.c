358edbbb209005f0b8e5ce0cc57bae29268f3a78
wh1314anyu <153305226+wh1314anyu@users.noreply.github.com>
2025-01-14 22:00:53 +0800

    Update thread.c

--- Lab3/kernel/object/thread.c
+++ Lab3/kernel/object/thread.c
@@ -232,25 +232,78 @@ void create_root_thread(void)
                 UNUSED(filesz);
                 UNUSED(offset);
                 UNUSED(memsz);
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_OFFSET_OFF),
+                       sizeof(data));
+                offset = (unsigned long)le64_to_cpu(*(u64 *)data);
 
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_VADDR_OFF),
+                       sizeof(data));
+                vaddr = (unsigned long)le64_to_cpu(*(u64 *)data);
+
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_FILESZ_OFF),
+                       sizeof(data));
+                filesz = (unsigned long)le64_to_cpu(*(u64 *)data);
+
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_MEMSZ_OFF),
+                       sizeof(data));
+                memsz = (unsigned long)le64_to_cpu(*(u64 *)data);
                 /* LAB 3 TODO END */
 
                 struct pmobject *segment_pmo = NULL;
                 /* LAB 3 TODO BEGIN */
                 UNUSED(segment_pmo);
+                size_t pmo_size = ROUND_UP(memsz, PAGE_SIZE);
 
+                vaddr_t segment_content_kvaddr =
+                        ((unsigned long)&binary_procmgr_bin_start) + offset;
+                BUG_ON(filesz != memsz);
+                // No additional memory for .bss, so we can directly reuse
+                // content in kernel image as their physical pages
+                ret = create_pmo(PAGE_SIZE,
+                                 PMO_DATA,
+                                 root_cap_group,
+                                 0,
+                                 &segment_pmo,
+                                 PMO_ALL_RIGHTS);
                 /* LAB 3 TODO END */
 
                 BUG_ON(ret < 0);
 
                 /* LAB 3 TODO BEGIN */
                 /* Copy elf file contents into memory*/
-
+                 kfree((void *)phys_to_virt(segment_pmo->start));
+#if CHCORE_ARCH_X86_64
+                // See comments of embedded_bin_virt_to_phys
+                segment_pmo->start =
+                        embedded_bin_virt_to_phys(segment_content_kvaddr);
+                segment_pmo->size = pmo_size;
+#else
+                segment_pmo->start = virt_to_phys(segment_content_kvaddr);
+                segment_pmo->size = pmo_size;
+#endif
                 /* LAB 3 TODO END */
 
                 unsigned vmr_flags = 0;
                 /* LAB 3 TODO BEGIN */
                 /* Set flags*/
+                if (flags & PHDR_FLAGS_R)
+                        vmr_flags |= VMR_READ;
+                if (flags & PHDR_FLAGS_W)
+                        vmr_flags |= VMR_WRITE;
+                if (flags & PHDR_FLAGS_X)
+                        vmr_flags |= VMR_EXEC;
 
                 /* LAB 3 TODO END */
 
