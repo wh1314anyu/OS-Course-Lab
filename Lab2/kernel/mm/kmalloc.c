c8e65e9ce02cd20f4cbf711d59478e7c45dd5811
wh1314anyu <153305226+wh1314anyu@users.noreply.github.com>
2025-01-14 21:23:59 +0800

    Update kmalloc.c

--- Lab2/kernel/mm/kmalloc.c
+++ Lab2/kernel/mm/kmalloc.c
@@ -117,8 +117,7 @@ void *_kmalloc(size_t size, bool is_record, size_t *real_size)
                 /* LAB 2 TODO 3 BEGIN */
                 /* Step 1: Allocate in slab for small requests. */
                 /* BLANK BEGIN */
-                UNUSED(addr);
-                UNUSED(order);
+               addr = alloc_in_slab(size, real_size);
 
                 /* BLANK END */
 #if ENABLE_MEMORY_USAGE_COLLECTING == ON
@@ -129,7 +128,11 @@ void *_kmalloc(size_t size, bool is_record, size_t *real_size)
         } else {
                 /* Step 2: Allocate in buddy for large requests. */
                 /* BLANK BEGIN */
-
+		if (size <= BUDDY_PAGE_SIZE)
+                        order = 0;
+                else
+                        order = size_to_page_order(size);
+                addr = get_pages(order);
                 /* BLANK END */
                 /* LAB 2 TODO 3 END */
         }
